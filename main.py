f917a792b20953b5230af200d56b2d39964b82b9
Pierre Rochard <pierre@rochard.org>
2015-07-19 12:54:27 -0400

    Switched to Aymeric Augustin's Python 3 Websockets implementation

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from websocket import create_connection
-ws = create_connection("ws://echo.websocket.org/")
-print "Sending 'Hello, World'..."
-ws.send("Hello, World")
-print "Sent"
-print "Reeiving..."
-result =  ws.recv()
-print "Received '%s'" % result
-ws.close()
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import json
+
+@asyncio.coroutine
+def hello():
+    web_socket = yield from websockets.connect("wss://ws-feed.exchange.coinbase.com")
+    yield from web_socket.send('{"type": "subscribe", "product_id": "BTC-USD"}')
+    while True:
+        message = yield from web_socket.recv()
+        message = json.loads(message)
+        print(message['type'])
+        print("{}".format(message))
+    yield from web_socket.close()
+
+asyncio.get_event_loop().run_until_complete(hello())
