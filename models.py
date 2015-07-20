4567d98bee3445aa1d243484ba4584c6f8978a71
Pierre Rochard <pierre@rochard.org>
2015-07-20 08:47:43 -0400

    fixing sql uri string

--- models.py
+++ models.py
@@ -3,9 +3,10 @@ from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, Integer, Numeric, String
+from urllib.parse import quote
+from config import DB_PASSWORD
 
-
-engine = create_engine("postgresql://Rochard@localhost/bigdipper")
+engine = create_engine("postgresql://pierre:%s@localhost/bigdipper" % quote(DB_PASSWORD))
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
